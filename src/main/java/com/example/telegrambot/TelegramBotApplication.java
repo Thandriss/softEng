0d2677a3dc18fe91cf82c01c6ea59b2fd1045bc1
Yana Makhonko <yana_mahonko@mail.ru>
2022-12-08 23:00:11 +0300

    Issue #67 new annotation for tests run

--- src/main/java/com/example/telegrambot/TelegramBotApplication.java
+++ src/main/java/com/example/telegrambot/TelegramBotApplication.java
@@ -1,12 +1,17 @@
 package com.example.telegrambot;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+//@SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan
 public class TelegramBotApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(TelegramBotApplication.class, args);
     }
 
